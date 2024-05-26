edf3c9badb7333977bcd051a2a4fd1648c5cf8bb
GabrielCoelho <gbcoe@outlook.com.br>
2024-05-26 13:51:10 -0300

    Formatação do Código

--- Projeto_ILP010_2024/Cliente.h
+++ Projeto_ILP010_2024/Cliente.h
@@ -2,13 +2,13 @@
 #define CLIENTE_H
 
 typedef struct {
-  int codigo_cliente; // Código será automático
-  char nome_cliente[15];
-  char sobrenome_cliente[20]; // String
-  int conta_corrente;         // Conta com 6 digitos
-  int senha_conta; // Número da agência setado no início do programa
-  double saldo_atual;
-  int chave_pix; // numero da agencia + codigo_cliente
+    int codigo_cliente;  // Código será automático
+    char nome_cliente[15];
+    char sobrenome_cliente[20];  // String
+    int conta_corrente;          // Conta com 6 digitos
+    int senha_conta;  // Número da agência setado no início do programa
+    double saldo_atual;
+    int chave_pix;  // numero da agencia + codigo_cliente
 } Cliente;
 
-#endif // !CLIENTE_H
+#endif  // !CLIENTE_H
